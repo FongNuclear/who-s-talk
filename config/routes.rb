6d2f834861f7b9922188febb6b668acc7972d1c5
Fong Nuclear <soe094004@gmail.com>
2018-12-11 08:46:57 +0800

    新增webhook

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
   	get '/whostalk/eat', to: 'whostalk#eat'
+  	get '/whostalk/request_headers', to: 'whostalk#request_headers'
+  	get '/whostalk/request_body', to: 'whostalk#request_body'
+  	get '/whostalk/response_headers', to: 'whostalk#response_headers'
+  	get '/whostalk/response_body', to: 'whostalk#show_response_body'
+  	get '/whostalk/sent_request', to: 'whostalk#sent_request'
+  	post '/whostalk/webhook', to: 'whostalk#webhook'
   end
 end
